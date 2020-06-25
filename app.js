98edf001eb34ebe436411a2a2da1b04045b87de8
gt20-ashish <65654865+gt20-ashish@users.noreply.github.com>
2020-06-25 11:10:17 +0530

    modifing app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+document.addEventListener("DOMContentLoaded",() => {
+    var cards = [{
+        name: "sample1",
+        img:"images/m1.png",
+    },{
+        name: "sample1",
+        img:"images/m1.png",
+    },{
+        name: "sample2",
+        img:"images/m2.png",
+    },{
+        name: "sample2",
+        img:"images/m2.png",
+    },{
+        name: "sample3",
+        img:"images/m3.png",
+    },{
+        name: "sample3",
+        img:"images/m3.png",
+    }];
+    
+    var grid = document.querySelector(".grid");
+
+    for(let i=0;i<cards.length;i++){
+        let newcard = createElement("img");
+        newcard.setAttribute("src","images/default.png");
+        newcard.setAttribute("id",i);
+        newcard.setAttribute("onclick","flipCard");
+        grid.appendChild(newcard);
+    }
+});
