831c0d3db6f47294491c5ab26e170a4f56fe9e3a
gt20-ashish <65654865+gt20-ashish@users.noreply.github.com>
2020-06-25 11:22:56 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,6 +21,16 @@ document.addEventListener("DOMContentLoaded",() => {
     
     var grid = document.querySelector(".grid");
     var cardChoosen = [];
+    
+     function flipCard(){
+        let cardSelected = this.getAttribute("id");
+        cardChoosen.push(cards[cardSelected].name);
+        this.setAttribute("src",cards[cardSelected].img);
+        if(cardSelected.length==2){
+            checkMatch();
+        }
+    }
+    
     for(let i=0;i<cards.length;i++){
         let newcard = document.createElement("img");
         newcard.setAttribute("src","images/default.png");
@@ -29,12 +39,5 @@ document.addEventListener("DOMContentLoaded",() => {
         grid.appendChild(newcard);
     }
 
-    function flipCard(){
-        let cardSelected = this.getAttribute("id");
-        cardChoosen.push(cards[cardSelected].name);
-        this.setAttribute("src",cards[cardSelected].img);
-        if(cardSelected.length==2){
-            checkMatch();
-        }
-    }
+   
 });
